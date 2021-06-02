0f908ee4d8ee745ce8924934f377da37f4b2302a
harris1111 <52365112+harris1111@users.noreply.github.com>
2021-06-02 21:21:25 +0700

    First Initialize

---
+++ Server/src/com/p2p/p4f/server/Main.java
@@ -0,0 +1,11 @@
+package com.p2p.p4f.server;
+
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
